7f6c84b236a486986687c549e635d7932e040847
Anish9345 <cocanish1234@gmail.com>
2026-07-08 14:36:04 +0530

    Added nav bar and router link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Signup } from './pages/signup/signup';
+import { StudentDashboard } from './pages/student-dashboard/student-dashboard';
+import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: Home
+    },
+    {
+        path: "login",
+        component: Login
+    },
+    {
+        path: "signup",
+        component: Signup
+    },
+    {
+        path: "student",
+        component: StudentDashboard
+    },
+    {
+        path: "admin",
+        component: AdminDashboard
+    }
+];
