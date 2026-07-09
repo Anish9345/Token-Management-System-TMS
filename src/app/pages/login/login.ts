93186a423c878d06477096cc8b5d3b93790e2db0
Anish9345 <cocanish1234@gmail.com>
2026-07-09 10:52:03 +0530

    writed and improved login.ts - used forms validators, and login.html

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { DatabaseService } from '../../services/database.service';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  
+  private fb = inject(FormBuilder);
+  private db = inject(DatabaseService);
+  private router = inject(Router);
+  
+  errorMessage: string = '';
+
+  reactiveForm: FormGroup = this.fb.group({
+    userId: [
+      "",
+      [
+        Validators.required,
+        Validators.minLength(2)
+      ]
+    ]
+  })
+
+  inValid(field: string): boolean{
+    const ctr = this.reactiveForm.get(field);
+    return !!ctr && ctr.invalid && (ctr.dirty || ctr.touched);
+  }
+
+  get f(){
+    return this.reactiveForm.controls;
+  }
+
+  onLogin(){
+    this.errorMessage = '';
+
+    if(this.reactiveForm.invalid){
+      this.reactiveForm.markAllAsTouched();
+      return;
+    }
+
+    const typedId = this.reactiveForm.value.userId;
+    const foundUser = this.db.users.find(u => u.id === typedId);
+
+    if(foundUser){
+      if(foundUser.role === 'Admin'){
+        this.router.navigate(['/admin']);
+      }else if(foundUser.role == 'Teacher'){
+        this.router.navigate(['/teacher']);
+      }else if(foundUser.role === 'Student'){
+        this.router.navigate(['/student']);
+      }else{
+        this.errorMessage = 'Your account is pending approval.';
+      }
+    }else{
+      this.errorMessage = 'Invalid User ID.';
+    }
+  }
+
+}
